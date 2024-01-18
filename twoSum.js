602fd4ac761654892219d275e41e803100ec52a1
amjido-01 <youndsadeeq10@gmail.com>
2024-01-18 07:59:15 +0100

    Working on the Bio

--- twoSum.js
+++ twoSum.js
@@ -1,33 +1,16 @@
-const nums = [1, 2, 3, 8];
-const target = 5;
+const nums = [2,11,15, 7];
+const target = 9;
 
-// const twoSum = (arr, tar) => {
-//     let vals = [];
-
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = i + 1; j < arr.length; j++) {
-//             if (arr[i] + arr[j] === tar) {
-//                 vals.push(i, j)
-//                 return vals
-//             }
-//         }
-//     }
-// }
-
-// const result = twoSum(nums, target)
-// console.log(result)
-
-const twoSum = (arr, tar) => {
-    const map = {};
-    for (let i = 0; i < arr.length; i++) {
-        map[arr[i]] = i;
-        let diff = tar - arr[i];
-        if (map[diff] !== undefined) {
-            return [numObj[complement], i];
-        }
-        numObj[nums[i]] = i;
-    }
+const twoSum = (arr, target) => {
+    let map = {};
+  for (let i = 0; i < nums.length; i++) {
+      const difference = target - nums[i];
+      if (map[difference] !== undefined) {
+          return [map[difference], i]
+      }
+      map[nums[i]] = i;
+  }
 }
 
 const result = twoSum(nums, target)
-console.log(result)
+console.log(result);
