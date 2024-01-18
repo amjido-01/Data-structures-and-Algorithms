18abdc863513f974f2c27be8460a04200c9af73a
amjido-01 <youndsadeeq10@gmail.com>
2024-01-18 12:26:34 +0100

    Working on the Bio

--- twoSum.js
+++ twoSum.js
@@ -3,14 +3,18 @@ const target = 9;
 
 const twoSum = (arr, target) => {
     let map = {};
-  for (let i = 0; i < nums.length; i++) {
-      const difference = target - nums[i];
-      if (map[difference] !== undefined) {
-          return [map[difference], i]
-      }
-      map[nums[i]] = i;
-  }
+
+    for (let i = 0; i < arr.length; i++) {
+        let diff = target - arr[i];
+        if (map[diff] !== undefined) {
+            return [map[diff], i]
+        }
+        map[arr[i]] = i
+    }
 }
 
 const result = twoSum(nums, target)
 console.log(result);
+
+
+//
