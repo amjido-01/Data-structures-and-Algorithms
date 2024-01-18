9567c2c5197171a7ff91894b93ef5f4d1ec2c17f
amjido-01 <youndsadeeq10@gmail.com>
2024-01-18 12:30:21 +0100

    twosum

--- twoSum.js
+++ twoSum.js
@@ -17,4 +17,18 @@ const result = twoSum(nums, target)
 console.log(result);
 
 
-//
+// brute force
+// const nums = [2,11,15, 7];
+
+const twoSum2 = (arr, tar) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j <arr.length; j++) {
+            if (arr[i] + arr[j] === tar) {
+                return [ i, j ]
+            }
+        }
+    }
+}
+
+const result2 = twoSum2(nums, target)
+console.log(result2);
