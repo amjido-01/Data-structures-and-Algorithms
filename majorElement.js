a564ad8c9615f9900eaab1bb28133b33c499b0f6
amjido-01 <youndsadeeq10@gmail.com>
2024-01-22 15:29:21 +0100

    aminated arrow

--- majorElement.js
+++ majorElement.js
@@ -2,7 +2,8 @@ const nums = [2,2,1,1,1,2,2,2]
 
 const major = (nums) => {
     const sorted = nums.sort((a, b) => a - b );
-    console.log(sorted);
+    const majorElement = sorted[nums.length/2];
+    console.log(majorElement);
 }
 
 
