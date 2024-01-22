cce5593637bd365edb684e797ed1cbcb6e5dd092
amjido-01 <youndsadeeq10@gmail.com>
2024-01-22 15:46:04 +0100

    major element in an array

--- majorElement.js
+++ majorElement.js
@@ -1,10 +1,14 @@
 const nums = [2,2,1,1,1,2,2,2]
+const nums2 = [3,2,3]
 
-const major = (nums) => {
-    const sorted = nums.sort((a, b) => a - b );
-    const majorElement = sorted[nums.length/2];
-    console.log(majorElement);
-}
+var majorityElement = function(nums) {
+    const sorted = nums.sort((a, b) => a - b);
+    const numsLength = nums.length/2;
+    const actual = Math.floor(numsLength);
+    const majorElement = sorted[actual]
+    return majorElement;
+};
 
 
-major(nums)
+let result = majorityElement(nums2)
+console.log(result);
