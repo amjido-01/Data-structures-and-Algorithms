68f0cfe3fe22a0e2f72ec99cf3a05e269ef78dab
amjido-01 <youndsadeeq10@gmail.com>
2024-01-24 07:03:07 +0100

    services section

--- sellStock.js
+++ sellStock.js
@@ -1,15 +1,44 @@
-const prices = [7,1,5,3,6,4];
+const prices = [7,1,5,3,6,4]
+const pricesTwo = [7,6,4,3,1]
 
-var maxProfit = function(prices) {
-    let lowest = prices[0]; // 7
+const maxProfit = (prices) => {
+    let minPrice = prices[0]
+    let maxProfit = 0;
 
     for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < lowest) {
-            lowest = prices[i]
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]
+        } else {
+            const potentialProfit = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit, potentialProfit)
         }
     }
-    // return lowest
-};
+    return maxProfit
+}
 
-const result = maxProfit(prices)
+
+const result = maxProfit(pricesTwo)
 console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+// var maxProfit = function(prices) {
+//     let maxval = 0;
+//     for (let i = 0; i < prices.length; i++) {
+//         for (let j = i+1; j < prices.length; j++) {
+//             maxval = Math.max(maxval, prices[j] - prices[i])
+//         }
+//     }
+//     return maxval
+// };
+
+// const result = maxProfit(prices)
+// console.log(result);
