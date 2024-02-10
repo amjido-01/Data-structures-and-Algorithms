a66e1e6de3f4670ae4d592dec9a95cf07a08e54c
amjido-01 <youndsadeeq10@gmail.com>
2024-02-10 14:57:38 +0100

    vidoe player

--- LastWord.js
+++ LastWord.js
@@ -4,15 +4,8 @@ const wordsTwo = "   fly me   to   the moon  "
 
 const lengthOfLastWord = (sentence) => {
     const splited = sentence.split(" ");
-    let arr = []
-
-    for (let i = 0; i < splited.length; i++) {
-        if (splited[i] !== '') {
-            arr.push(splited[i])
-        }
-    }
-    let last = arr.length-1
-    return arr[last].length
+    const arranged = splited.filter(item => item !== '')
+    return arranged[arranged.length-1].length
 }
 
 const result = lengthOfLastWord(wordsTwo)
