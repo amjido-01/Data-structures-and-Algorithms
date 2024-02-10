a427bae459ef1f1366b919323da22f609ebccc6e
amjido-01 <youndsadeeq10@gmail.com>
2024-02-10 15:15:44 +0100

    vidoe player

--- margeArray.js
+++ margeArray.js
@@ -19,6 +19,23 @@ var merge = function(nums1, m, nums2, n) {
 // merge(nums1, 3, nums2, 3)
 // nums1, m, nums2, n
 var merge2 = function (nums1, m, nums2, n) {
+    let i = m-1
+    let j = n-1
+    let k = m+n -1;
+    while(i >= 0 & j >= 0) {
+       if (nums1[i] > nums2[j]) {
+        nums1[k--] = nums1[i--]
+       } else{
+        nums1[k--] = nums2[j--]
+       }
+    }
+    // return nums1;
+    console.log(nums1);
+
+    while(j >= 0) {
+        nums1[k--] = nums2[j]
+    }
 }
 
 
+merge2(nums1, 3, nums2, 3)
