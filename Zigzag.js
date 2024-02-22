c890f2f8bc6a923c570612a422acefbabb9b5ad2
amjido-01 <youndsadeeq10@gmail.com>
2024-02-22 11:19:56 +0100

    working on the login form

--- Zigzag.js
+++ Zigzag.js
@@ -2,14 +2,14 @@ function convert(s, numRows) {
     if (numRows === 1) return s; 
   
     const rows = Array(numRows).fill(''); // Array to store characters in each row
-    // ['', '', '']
-    let currentRow = 0;   // 
-    let goingDown = true; //  
+    // ['PAHG', 'APLSIN', 'YIRI']
+    let currentRow = 0;   // 1, 2, 1, 0, 1, 2, 1, 0, 1, 2, 1, 0
+    let goingDown = true; // T, F, F, T, F, F, T, F, F, F, T
   
     for (let i = 0; i < s.length; i++) {
       rows[currentRow] += s[i]; // Add character to current row
-    //  P, A, Y P A, L, I S H I R I N G
-    //  0  1  2 3 4  5  6
+    //  P, A, Y, P, A, L, I, S, H, I, R, I, N, G
+    //  0  1  2  3  4  5  6  7  8  9  10  11 12
 
 
       // Determine next row based on zigzag pattern
