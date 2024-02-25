0457ea002623696cdfea90151cb62100e1f40e6b
amjido-01 <youndsadeeq10@gmail.com>
2024-02-25 14:28:54 +0100

    commit

--- Hashmap/WordPattern.js
+++ Hashmap/WordPattern.js
@@ -1,53 +1,30 @@
-// const pattern = "abba"
-// const s = "dog cat cat dog"
-// const pattern = "abba"
-// const s = "dog cat cat fish"
-const s = "aba"
-const pattern ="dog cat cat"
-// const pattern = "aaaa", s = "dog cat cat dog"
+const pattern = "abba"
+const s = "dog cat cat dog"
+// const pattern = "abba", s = "dog cat cat fish"
 
 var wordPattern = function(pattern, s) {
-    let splittedPattern = pattern.split(' ')
-    if (splittedPattern.length !== s.length) {
+    const words = s.split(' ');
+  
+    if (pattern.length !== words.length) {
+      return false;
+    }
+  
+    const sMap = {};
+    const pMap = {};
+  
+    for (let i = 0; i < pattern.length; i++) {
+      if (sMap[words[i]] && sMap[words[i]] !== pattern[i]) {
         return false;
+      }
+      if (pMap[pattern[i]] && pMap[pattern[i]] !== words[i]) {
+        return false;
+      }
+      sMap[words[i]] = pattern[i];
+      pMap[pattern[i]] = words[i]
     }
-    let pMap = {};
-    let sMap = {};
-
-    for (let i = 0; i < splittedPattern.length; i++) {
-        pMap[splittedPattern[i]] = (pMap[splittedPattern[i]] || 0) + 1
-    }
-    
-    for (let i = 0; i < s.length; i++) {
-        sMap[s[i]] = (sMap[s[i]] || 0) + 1
-    }
-    console.log(sMap);
-    
-};
+  
+    return true;
+  };
 
 let res = wordPattern(pattern, s)
 console.log(res);
-
-
-
-
-// for (let i = 0; i < s.length; i++) {
-//     pMap[pattern[i]] = (pMap[pattern[i]] || 0) + 1
-// }
-
-
-// for (let i = 0; i < splittedS.length; i++) {
-//     sMap[splittedS[i]] = (sMap[splittedS[i]] || 0) + 1
-// }
-
-// let sLen = Object.keys(sMap).length
-// let pLen = Object.keys(pMap).length
-// console.log(sLen, pLen);
-// console.log(pMap, sMap);
-
-// for (let i = 0; i < s.length; i++) {
-//     if (sLen !== pLen || sMap[s[i]] && sMap[s[i]] !== pattern[i]) {
-//         return false
-//     } 
-// }
-// return true
