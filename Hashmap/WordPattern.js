db0ac0f618d681d200a676e7a2b1eee44d0b9ad7
amjido-01 <youndsadeeq10@gmail.com>
2024-02-25 20:50:38 +0100

    word pattern

--- Hashmap/WordPattern.js
+++ Hashmap/WordPattern.js
@@ -3,28 +3,27 @@ const s = "dog cat cat dog"
 // const pattern = "abba", s = "dog cat cat fish"
 
 var wordPattern = function(pattern, s) {
-    const words = s.split(' ');
-  
-    if (pattern.length !== words.length) {
-      return false;
-    }
-  
-    const sMap = {};
-    const pMap = {};
-  
+    const splitedStr = s.split(" ")
+    
+    if (pattern.length !== splitedStr.length) return false;
+
+    const sMap = new Map();
+    const pMap = new Map();
+
     for (let i = 0; i < pattern.length; i++) {
-      if (sMap[words[i]] && sMap[words[i]] !== pattern[i]) {
-        return false;
+      if (sMap.has(splitedStr[i]) && sMap.get(splitedStr[i]) !== pattern[i]){
+        return false
       }
-      if (pMap[pattern[i]] && pMap[pattern[i]] !== words[i]) {
-        return false;
+      if (pMap.has(pattern[i]) && pMap.get(pattern[i]) !== splitedStr[i]) {
+        return false
       }
-      sMap[words[i]] = pattern[i];
-      pMap[pattern[i]] = words[i]
+
+      sMap.set(splitedStr[i], pattern[i])
+      pMap.set(pattern[i], splitedStr[i])
     }
-  
-    return true;
-  };
+    return true
+
+};
 
 let res = wordPattern(pattern, s)
 console.log(res);
