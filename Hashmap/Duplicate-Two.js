063b94042498b5b57ca5ac9a344e2340f861dd81
amjido-01 <youndsadeeq10@gmail.com>
2024-02-26 20:19:20 +0100

    commit

--- Hashmap/Duplicate-Two.js
+++ Hashmap/Duplicate-Two.js
@@ -1,8 +1,32 @@
 const nums = [1,2,3,1], k = 3
+// const nums = [1,0,1,1], k = 1
+// const nums = [1,2,3,1,2,3], k = 2
+
+// var containsNearbyDuplicate = function(nums, k) {
+//     const hashMap = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         if (hashMap[i] && Math.abs(i - hashMap[i]) <= k) {
+//             return true
+//         } else {
+//             return false
+//         }
+//     }
+//     hashMap[i] = i;
+// };
+
+
+
+
+
+
+
+
+
+
 var containsNearbyDuplicate = function(nums, k) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i +1; j < nums.length; j++) {
-            if (nums[i] == nums[j] && Math.abs(j - i) == k){
+            if (nums[i] == nums[j] && Math.abs(j - i) <= k){
                 return true
             }
         }
