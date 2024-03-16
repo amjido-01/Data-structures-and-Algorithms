bbf976a6df4efb4788c9147e15de3731fafab42b
amjido-01 <youndsadeeq10@gmail.com>
2024-03-16 09:02:13 +0100

    commit

--- Array/majorElement.js
+++ Array/majorElement.js
@@ -2,11 +2,8 @@ const nums = [2,2,1,1,1,2,2,2]
 const nums2 = [3,2,3]
 
 var majorityElement = function(nums) {
-    const sorted = nums.sort((a, b) => a - b);
-    const numsLength = nums.length/2;
-    const actual = Math.floor(numsLength);
-    const majorElement = sorted[actual]
-    return majorElement;
+   const sorted = nums.sort((a, b) => a -b)
+   console.log(sorted);
 };
 
 
