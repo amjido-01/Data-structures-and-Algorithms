576c80dbbfbbb8ac1884078b0e0812c0fb5e12e3
amjido-01 <youndsadeeq10@gmail.com>
2024-03-31 08:55:12 +0100

    commit

--- Array/canCompleteCircuit.js
+++ Array/canCompleteCircuit.js
@@ -19,8 +19,8 @@ function canCompleteCircuit(gas, cost) {
    for (let i = 0; i < gas.length; i++) {
     tank += gas[i] - cost[i]
     if (tank < 0) {
-        start = i + 1
         tank = 0
+        start = i + 1
     }
    }
    return start
--- Array/sandbox.js
+++ Array/sandbox.js
@@ -1,38 +1,67 @@
-class GetRandom {
-    constructor() {
-        this.map = {}
-        this.arr = []
-    }
+// class GetRandom {
+//     constructor() {
+//         this.map = {}
+//         this.arr = []
+//     }
 
-    insert(val) {
-        if(this.map[val] !== undefined) {
-            return false
+//     insert(val) {
+//         if(this.map[val] !== undefined) {
+//             return false
+//         }
+
+//         this.arr.push(val)
+//         this.map[val] = this.arr.length-1
+//         return true
+//     }
+
+//     remove(val) {
+//         if (this.map[val] === undefined) {
+//             return false
+//         }
+
+//         const lastIdx = this.arr.length-1
+//         const idx = this.map[val]
+//         const lastVal = this.arr[lastIdx]
+
+//         this.arr[idx] = lastVal
+//         this.map[lastVal] = idx
+
+//         delete this.map[val]
+//         this.arr.pop()
+//         return true
+//     }
+
+//     getRandomize() {
+//         const rand = Math.floor(Math.random() * this.arr.length)
+//         return this.arr[randomIndex];
+//     }
+// }
+
+
+
+var romanToInt = function(s) {
+    const map = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000,
         }
-
-        this.arr.push(val)
-        this.map[val] = this.arr.length-1
-        return true
-    }
-
-    remove(val) {
-        if (this.map[val] === undefined) {
-            return false
+        
+        let res = 0;
+        for (let i = 0; i < s.length; i++) {
+            console.log(map[s[i]]);
+            if (map[s[i+1]] > map[s[i]]) {
+                res += map[s[i+1]] - map[s[i]]
+                i++
+            } else {
+                res += map[s[i]]
+            }
         }
+        return res
+};
 
-        const lastIdx = this.arr.length-1
-        const idx = this.map[val]
-        const lastVal = this.arr[lastIdx]
-
-        this.arr[idx] = lastVal
-        this.map[lastVal] = idx
-
-        delete this.map[val]
-        this.arr.pop()
-        return true
-    }
-
-    getRandomize() {
-        const rand = Math.floor(Math.random() * this.arr.length)
-        return this.arr[randomIndex];
-    }
-}
+let val = romanToInt('MCMXCIV')
+console.log(val);
