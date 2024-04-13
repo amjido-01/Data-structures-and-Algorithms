bcdc5330c0e69dd78ac5d18f397ac8761f1a6c3c
amjido-01 <youndsadeeq10@gmail.com>
2024-04-13 21:39:41 +0100

    linked list

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -14,7 +14,14 @@
 // }
 
 class linkedList {
-    
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null
+        }
+        this.tail = this.head
+        this.length = 1
+    }
 }
 
 const myLinkedList = new linkedList(10)
