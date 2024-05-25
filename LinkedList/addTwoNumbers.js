7f5287dd127d2dd07e9304f6955def76fcb2780c
amjido-01 <youndsadeeq10@gmai;.com>
2024-05-25 14:33:09 +0100

    commit

--- LinkedList/addTwoNumbers.js
+++ LinkedList/addTwoNumbers.js
@@ -5,34 +5,57 @@ class ListNode {
     }
 
     addTwoNumbers(l1, l2) {
-        let list = new ListNode(0);
+        let list = new ListNode(0)
         let head = list;
-        let carry = 0;
         let sum = 0;
-
-        while(li !== null || l2 !== null || sum > 0) {
+        let carry = 0;
+    
+        while(l1 !== null || l2 !== null || sum > 0) {
             if (l1 !== null) {
-                sum += l1.value;
-                l1 = l1.next;
+                sum += l1.value
+                l1 = l1.next
             }
-
+    
             if (l2 !== null) {
-                sum += l2.value;
+                sum += l2.value
                 l2 = l2.next
             }
-
+    
             if (sum >= 10) {
                 carry = 1;
-                sum = sum - 10;
+                sum -= 10
+            } else {
+                carry = 0;
             }
-
-            head.next = new ListNode(sum);
+    
+            head.next = new ListNode(sum)
             head = head.next
-
+    
             sum = carry;
             carry = 0
         }
-        return list.next;
+        const result = list.next;
+        result.printList(); // Print the resulting linked list
+        return result;
+    };
+    printList() {
+        let current = this;
+        let result = '';
+        while (current !== null) {
+            result += current.value + ' -> ';
+            current = current.next;
+        }
+        result += 'null';
+        console.log(result);
     }
-
 }
+const node1 = new ListNode(2);
+node1.next = new ListNode(4);
+node1.next.next = new ListNode(3);
+
+const node2 = new ListNode(5);
+node2.next = new ListNode(6);
+node2.next.next = new ListNode(4);
+
+const result = new ListNode();
+result.addTwoNumbers(node1, node2);
