0aaadf20a9487422f64894bca1e01549e9066cd0
amjido-01 <youndsadeeq10@gmai;.com>
2024-06-20 11:57:36 +0100

    stack

--- Stack/stack.js
+++ Stack/stack.js
@@ -1,80 +1,82 @@
-// class Node {
-//     constructor(value) {
-//       this.value = value;
-//       this.next = null;
-//     }
-//   }
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
   
-//   class Stack {
-//     constructor() {
-//       this.top = null;
-//       this.bottom = null;
-//       this.length = 0;
-//     }
-//     peek() {
-//       return this.top;
-//     }
-//     push(value) {
-//       const newNode = new Node(value);
-//       if (this.length === 0) {
-//         this.top = newNode;
-//         this.bottom = newNode;
-//       } else {
-//         const holdingPointer = this.top;
-//         this.top = newNode;
-//         this.top.next = holdingPointer;
-//       }
-//       this.length++;
-//       return this;
-//     }
-//     pop() {
-//       if (!this.top) {
-//         return null;
-//       }
-//       if (this.top === this.bottom) {
-//         this.bottom = null;
-//       }
-//       this.top = this.top.next;
-//       this.length--;
-//       return this;
-//     }
-//   }
-  
-//   const myStack = new Stack();
-//   console.log(myStack.push("google"));
-//   console.log(myStack.push("google2"));
-//   console.log(myStack.peek());
-//   console.log(myStack.pop());
-//   console.log(myStack.pop());
-
-
-
   class Stack {
-    constructor(){
-      this.array = [];
+    constructor() {
+      this.top = null;
+      this.bottom = null;
+      this.length = 0;
     }
     peek() {
-      return this.array[this.array.length-1];
+      return this.top;
     }
-    push(value){
-      this.array.push(value);
+    push(value) {
+      const newNode = new Node(value);
+      if (this.length === 0) {
+        this.top = newNode;
+        this.bottom = newNode;
+      } else {
+        const holdingPointer = this.top;
+        this.top = newNode;
+        this.top.next = holdingPointer;
+        console.log(this.top, "this.top");
+        console.log(holdingPointer, "holdingPointer");  
+      }
+      this.length++;
       return this;
     }
-    pop(){
-      this.array.pop();
+    pop() {
+      if (!this.top) {
+        return null;
+      }
+      if (this.top === this.bottom) {
+        this.bottom = null;
+      }
+      this.top = this.top.next;
+      this.length--;
       return this;
     }
   }
   
   const myStack = new Stack();
-  myStack.peek();
-  myStack.push('google');
-  myStack.push('ztm');
-  myStack.push('discord');
-  myStack.peek();
-  myStack.pop();
-  myStack.pop();
-  myStack.pop();
+  console.log(myStack.push("google"));
+  console.log(myStack.push("google2"));
+  console.log(myStack.peek());
+//   console.log(myStack.pop());
+//   console.log(myStack.pop());
+
+
+
+//   class Stack {
+//     constructor(){
+//       this.stack = [];
+//     }
+//     peek() {
+//       return this.stack[this.stack.length-1];
+//     }
+//     push(value){
+//       this.stack.push(value);
+//       return this;
+//     }
+//     pop(){
+//       this.stack.pop();
+//       return this;
+//     }
+//   }
+  
+//   const myStack = new Stack();
+//   myStack.peek();
+//   myStack.push('google');
+//   myStack.push('ztm');
+//   myStack.push('discord');
+//   myStack.peek();
+//   myStack.pop();
+//   myStack.pop();
+//   myStack.pop();
   
   
   //Discord
