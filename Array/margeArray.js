d1d22d70cc9cae23b58f2890616400e9b5764647
amjido-01 <youndsadeeq10@gmail.com>
2024-07-25 22:44:46 +0100

    tree

--- Array/margeArray.js
+++ Array/margeArray.js
@@ -39,3 +39,18 @@ var merge2 = function (nums1, m, nums2, n) {
 
 
 merge2(nums1, 3, nums2, 3)
+
+
+var mergee = function(nums1, m, nums2, n) {
+    let i = m-1;
+    let j = n-1;
+    let k = m + n -1;
+
+    while(j >= 0) {
+        if(i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--]
+        } else {
+             nums1[k--] = nums2[j--];
+        }
+    }
+};
